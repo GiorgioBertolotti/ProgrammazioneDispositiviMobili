69c912c53bae5364c8f9bedc007a8fc6460f3f1e
TiaLanzi <lanzimattia.98@gmail.com>
2020-04-21 16:55:20 +0200

    :construction: Created bug report activity

---
+++ QuakeApp/app/src/main/java/it/unimib/quakeapp/BugReport.java
@@ -0,0 +1,14 @@
+package it.unimib.quakeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BugReport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bug_report);
+    }
+}
