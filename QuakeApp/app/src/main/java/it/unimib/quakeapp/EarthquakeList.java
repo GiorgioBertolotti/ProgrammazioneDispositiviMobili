8220b0a229f5ae30ec7d03d90cce09ececdb18db
Giorgio Bertolotti <giorgiobertol@gmail.com>
2020-04-26 18:15:29 +0200

    :sparkles: Added sections header, added sorting functions by date, richter and mercalli

--- QuakeApp/app/src/main/java/it/unimib/quakeapp/EarthquakeList.java
+++ QuakeApp/app/src/main/java/it/unimib/quakeapp/EarthquakeList.java
@@ -13,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -21,10 +22,18 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.TreeSet;
 
 import it.unimib.quakeapp.models.Earthquake;
 import okhttp3.OkHttpClient;
@@ -35,10 +44,16 @@ import static it.unimib.quakeapp.MainActivity.TAG;
 
 
 public class EarthquakeList extends Fragment {
-    private final String EARTHQUAKE_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&limit=100&eventtype=earthquake";
-    private ArrayList<Earthquake> earthquakes = new ArrayList<>();
-    private EarthquakeListAdapter listAdapter;
+    enum SortBy {
+        DATE,
+        RICHTER,
+        MERCALLI
+    }
+
+    private final String EARTHQUAKE_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&limit=230&eventtype=earthquake";
+    private EarthquakeAdapter listAdapter;
     private ListView earthquakeList;
+    private SortBy sortMethod = SortBy.DATE;
 
     public EarthquakeList() {
     }
@@ -60,7 +75,7 @@ public class EarthquakeList extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         this.earthquakeList = getView().findViewById(R.id.earthquake_list);
 
-        this.listAdapter = new EarthquakeListAdapter(getContext(), R.layout.earthquake_list_item, this.earthquakes);
+        this.listAdapter = new EarthquakeAdapter(getContext());
         this.earthquakeList.setAdapter(this.listAdapter);
     }
 
@@ -99,9 +114,72 @@ public class EarthquakeList extends Fragment {
                     JSONObject root = new JSONObject(result);
                     JSONArray features = root.getJSONArray("features");
 
+                    List<Earthquake> earthquakes = new ArrayList<>();
+
                     for (int i = 0; i < features.length(); i++) {
                         JSONObject feature = features.getJSONObject(i);
                         earthquakes.add(new Earthquake(feature));
+                    }
+
+                    switch (sortMethod) {
+                        case DATE: {
+                            Collections.sort(earthquakes, new DateComparator());
+                            List<Date> days = new ArrayList<>();
+
+                            for (int i = 0; i < earthquakes.size(); i++) {
+                                Earthquake earthquake = earthquakes.get(i);
+
+                                Calendar calendar = Calendar.getInstance();
+                                calendar.setTime(earthquake.time);
+                                calendar.set(Calendar.HOUR, 0);
+                                calendar.set(Calendar.HOUR_OF_DAY, 0);
+                                calendar.set(Calendar.MINUTE, 0);
+                                calendar.set(Calendar.SECOND, 0);
+                                calendar.set(Calendar.MILLISECOND, 0);
+                                Date day = calendar.getTime();
+
+                                if (!days.contains(day)) {
+                                    listAdapter.addSectionHeaderItem(earthquake);
+                                    days.add(day);
+                                }
+                                listAdapter.addItem(earthquake);
+                            }
+                            break;
+                        }
+                        case RICHTER: {
+                            Collections.sort(earthquakes, new RichterComparator());
+                            List<String> richters = new ArrayList<>();
+
+                            for (int i = 0; i < earthquakes.size(); i++) {
+                                Earthquake earthquake = earthquakes.get(i);
+
+                                String richter = Integer.toString((int) Math.floor(earthquake.richter_mag));
+
+                                if (!richters.contains(richter)) {
+                                    listAdapter.addSectionHeaderItem(earthquake);
+                                    richters.add(richter);
+                                }
+                                listAdapter.addItem(earthquake);
+                            }
+                            break;
+                        }
+                        case MERCALLI: {
+                            Collections.sort(earthquakes, new MercalliComparator());
+                            List<String> mercallis = new ArrayList<>();
+
+                            for (int i = 0; i < earthquakes.size(); i++) {
+                                Earthquake earthquake = earthquakes.get(i);
+
+                                String mercalli = Integer.toString((int) Math.floor(earthquake.mercalli));
+
+                                if (!mercallis.contains(mercalli)) {
+                                    listAdapter.addSectionHeaderItem(earthquake);
+                                    mercallis.add(mercalli);
+                                }
+                                listAdapter.addItem(earthquake);
+                            }
+                            break;
+                        }
                     }
 
                     listAdapter.notifyDataSetChanged();
@@ -121,48 +199,156 @@ public class EarthquakeList extends Fragment {
         }
     }
 
-    public class EarthquakeListAdapter extends ArrayAdapter<Earthquake> {
+    public class EarthquakeAdapter extends BaseAdapter {
 
-        public EarthquakeListAdapter(Context context, int textViewResourceId,
-                                     List<Earthquake> objects) {
-            super(context, textViewResourceId, objects);
+        private static final int TYPE_ITEM = 0;
+        private static final int TYPE_HEADER = 1;
+
+        private ArrayList<Earthquake> earthquakes = new ArrayList<Earthquake>();
+        private TreeSet<Integer> sectionHeader = new TreeSet<Integer>();
+
+        private LayoutInflater mInflater;
+
+        public EarthquakeAdapter(Context context) {
+            mInflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        public void addItem(final Earthquake item) {
+            earthquakes.add(item);
+            notifyDataSetChanged();
+        }
+
+        public void addSectionHeaderItem(final Earthquake item) {
+            earthquakes.add(item);
+            sectionHeader.add(earthquakes.size() - 1);
+            notifyDataSetChanged();
         }
 
         @Override
+        public int getItemViewType(int position) {
+            return sectionHeader.contains(position) ? TYPE_HEADER : TYPE_ITEM;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 2;
+        }
+
+        @Override
+        public int getCount() {
+            return earthquakes.size();
+        }
+
+        @Override
+        public Earthquake getItem(int position) {
+            return earthquakes.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
         public View getView(int position, View convertView, ViewGroup parent) {
-            LayoutInflater inflater = (LayoutInflater) getContext()
-                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.earthquake_list_item, null);
-
-            TextView eqiDate = convertView.findViewById(R.id.eqi_date);
-            TextView eqiLocation = convertView.findViewById(R.id.eqi_location);
-            TextView eqiRichterMag = convertView.findViewById(R.id.eqi_richter_mag);
-            TextView eqiMercalliMag = convertView.findViewById(R.id.eqi_mercalli_mag);
-
+            int rowType = getItemViewType(position);
             Earthquake earthquake = earthquakes.get(position);
 
-            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault());
-            eqiDate.setText(sdf.format(earthquake.time));
+            switch (rowType) {
+                case TYPE_ITEM: {
+                    convertView = mInflater.inflate(R.layout.earthquake_list_item, null);
 
-            String[] words = earthquake.place.split(" ");
-            String place = "";
+                    TextView eqiDate = convertView.findViewById(R.id.eqi_date);
+                    TextView eqiLocation = convertView.findViewById(R.id.eqi_location);
+                    TextView eqiRichterMag = convertView.findViewById(R.id.eqi_richter_mag);
+                    TextView eqiMercalliMag = convertView.findViewById(R.id.eqi_mercalli_mag);
+                    View eqiDivider = convertView.findViewById(R.id.eqi_divider);
 
-            if (words.length > 3) {
-                for (int i = 3; i < words.length; i++) {
-                    place += words[i] + " ";
+                    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault());
+                    eqiDate.setText(sdf.format(earthquake.time));
+
+                    String[] words = earthquake.place.split(" ");
+                    String place = "";
+
+                    if (words.length > 3) {
+                        for (int i = 3; i < words.length; i++) {
+                            place += words[i] + " ";
+                        }
+                    } else {
+                        place = earthquake.place;
+                    }
+
+                    eqiLocation.setText(place);
+                    String richter = Integer.toString((int) Math.floor(earthquake.richter_mag));
+                    String mercalli = Integer.toString((int) Math.floor(earthquake.mercalli));
+                    eqiRichterMag.setText(String.format("%s Richter", richter));
+                    eqiMercalliMag.setText(String.format("%s Mercalli", mercalli));
+
+                    if (position == earthquakes.size() - 1 || (earthquakes.size() > position + 1 && getItemViewType(position + 1) == TYPE_HEADER)) {
+                        eqiDivider.setVisibility(View.GONE);
+                    }
+                    break;
                 }
-            } else {
-                place = earthquake.place;
+                case TYPE_HEADER: {
+                    convertView = mInflater.inflate(R.layout.earthquake_list_header, null);
+
+                    TextView diHeaderTitle = convertView.findViewById(R.id.di_header_title);
+
+                    switch (sortMethod) {
+                        case DATE: {
+                            if (isSameDay(new Date(), earthquake.time)) {
+                                diHeaderTitle.setText(getString(R.string.today));
+                            } else {
+                                SimpleDateFormat sdf = new SimpleDateFormat("EEEE dd/MM/yyyy", Locale.getDefault());
+                                diHeaderTitle.setText(capitalizeFirst(sdf.format(earthquake.time)));
+                            }
+                            break;
+                        }
+                        case RICHTER: {
+                            String richter = Integer.toString((int) Math.floor(earthquake.richter_mag));
+                            diHeaderTitle.setText(String.format("Richter %s", richter));
+                            break;
+                        }
+                        case MERCALLI: {
+                            String mercalli = Integer.toString((int) Math.floor(earthquake.mercalli));
+                            diHeaderTitle.setText(String.format("Mercalli %s", mercalli));
+                            break;
+                        }
+                    }
+                    break;
+                }
             }
-
-            eqiLocation.setText(place);
-            String richter = Integer.toString((int) Math.floor(earthquake.richter_mag));
-            String mercalli = Integer.toString((int) Math.floor(earthquake.mercalli));
-            eqiRichterMag.setText(String.format("%s Richter", richter));
-            eqiMercalliMag.setText(String.format("%s Mercalli", mercalli));
-
             return convertView;
         }
 
+        boolean isSameDay(Date date1, Date date2) {
+            SimpleDateFormat fmt = new SimpleDateFormat("yyyyMMdd");
+            return fmt.format(date1).equals(fmt.format(date2));
+        }
+
+        String capitalizeFirst(String text){
+            return text.substring(0, 1).toUpperCase() + text.substring(1);
+        }
+    }
+
+    public class DateComparator implements Comparator<Earthquake> {
+        @Override
+        public int compare(Earthquake o1, Earthquake o2) {
+            return o2.time.compareTo(o1.time);
+        }
+    }
+
+    public class RichterComparator implements Comparator<Earthquake> {
+        @Override
+        public int compare(Earthquake o1, Earthquake o2) {
+            return Double.compare(o2.richter_mag, o1.richter_mag);
+        }
+    }
+
+    public class MercalliComparator implements Comparator<Earthquake> {
+        @Override
+        public int compare(Earthquake o1, Earthquake o2) {
+            return Double.compare(o2.mercalli, o1.mercalli);
+        }
     }
 }
