241e1d3db8db526aceb980af8930ee81eba2aee3
HudaChoudhry <hudachoudhry786@gmail.com>
2020-04-26 04:50:50 +0200

    still faq

--- QuakeApp/app/src/main/java/it/unimib/quakeapp/FAQ.java
+++ QuakeApp/app/src/main/java/it/unimib/quakeapp/FAQ.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -100,7 +101,85 @@ public class FAQ extends AppCompatActivity {
                 ans4.setVisibility(View.GONE);
             }
         });
+/////QUESTION5
+        final TextView ans5 = findViewById(R.id.answer5);
+        final ImageButton upToDown5 = findViewById(R.id.up_to_down5);
+        final ImageButton downToUp5 = findViewById(R.id.down_to_up5);
+        final ImageView diagram = findViewById(R.id.diagram);
 
+        upToDown5.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                upToDown5.setVisibility(View.GONE);
+                downToUp5.setVisibility(View.VISIBLE);
+                ans5.setVisibility(View.VISIBLE);
+                diagram.setVisibility(View.VISIBLE);
+            }
+        });
+        downToUp5.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                upToDown5.setVisibility(View.VISIBLE);
+                downToUp5.setVisibility(View.GONE);
+                ans5.setVisibility(View.GONE);
+                diagram.setVisibility(View.GONE);
+            }
+        });
+///////////QUESTION6
+        final TextView ans6 = findViewById(R.id.answer6);
+        final ImageButton upToDown6 = findViewById(R.id.up_to_down6);
+        final ImageButton downToUp6 = findViewById(R.id.down_to_up6);
+
+        upToDown6.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                upToDown6.setVisibility(View.GONE);
+                downToUp6.setVisibility(View.VISIBLE);
+                ans6.setVisibility(View.VISIBLE);
+            }
+        });
+        downToUp6.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                upToDown6.setVisibility(View.VISIBLE);
+                downToUp6.setVisibility(View.GONE);
+                ans6.setVisibility(View.GONE);
+            }
+        });
+///////////QUESTION7
+        final TextView ans7 = findViewById(R.id.answer7);
+        final ImageButton upToDown7 = findViewById(R.id.up_to_down7);
+        final ImageButton downToUp7 = findViewById(R.id.down_to_up7);
+
+        upToDown7.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                upToDown7.setVisibility(View.GONE);
+                downToUp7.setVisibility(View.VISIBLE);
+                ans7.setVisibility(View.VISIBLE);
+            }
+        });
+        downToUp7.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                upToDown7.setVisibility(View.VISIBLE);
+                downToUp7.setVisibility(View.GONE);
+                ans7.setVisibility(View.GONE);
+            }
+        });
+///////////QUESTION8
+        final TextView ans8 = findViewById(R.id.answer8);
+        final ImageButton upToDown8 = findViewById(R.id.up_to_down8);
+        final ImageButton downToUp8 = findViewById(R.id.down_to_up8);
+
+        upToDown8.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                upToDown8.setVisibility(View.GONE);
+                downToUp8.setVisibility(View.VISIBLE);
+                ans8.setVisibility(View.VISIBLE);
+            }
+        });
+        downToUp8.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                upToDown8.setVisibility(View.VISIBLE);
+                downToUp8.setVisibility(View.GONE);
+                ans8.setVisibility(View.GONE);
+            }
+        });
 
 
     }
