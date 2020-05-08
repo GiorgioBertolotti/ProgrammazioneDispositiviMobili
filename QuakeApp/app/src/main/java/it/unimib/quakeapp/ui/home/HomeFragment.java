26847e739e1355c5743828b15f514b42f5f4f0ee
TiaLanzi <lanzimattia.98@gmail.com>
2020-05-08 12:51:29 +0200

    :rocket: Created cards dahboard

--- QuakeApp/app/src/main/java/it/unimib/quakeapp/ui/home/HomeFragment.java
+++ QuakeApp/app/src/main/java/it/unimib/quakeapp/ui/home/HomeFragment.java
@@ -7,10 +7,10 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
+
 
 import it.unimib.quakeapp.R;
 
@@ -23,13 +23,17 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        TextView todayEQ = root.findViewById(R.id.home_number_tremors);
+        //todayEQ.setText();
+        TextView weekEQ = root.findViewById(R.id.home_number_tremors_week);
+        //weekEQ.setText();
+        /*final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
         return root;
     }
 }
