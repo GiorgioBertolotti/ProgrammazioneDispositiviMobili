fc26822ee7d5d4a04428a3ab7612530839fc318c
Faripod <n.farinato@campus.unimib.it>
2020-06-06 11:04:12 +0200

    :rocket: Created map using GoogleMaps

---
+++ QuakeApp/app/src/main/java/it/unimib/quakeapp/Maps.java
@@ -0,0 +1,38 @@
+package it.unimib.quakeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Maps extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng valma = new LatLng(45.8524827,9.3403053);
+        map.addMarker(new MarkerOptions().position(valma).title("Valma"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(valma));
+
+
+    }
+}
